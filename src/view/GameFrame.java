9d2767fc7d19e3a1178b85fbb10d96487f15e439
Phong Duong <phongduongg2102@gmail.com>
2026-03-15 18:44:48 +0700

    update player movement

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -1,5 +1,22 @@
 package BTL_brick_breaker_game.src.view;
+import javax.swing.JFrame;
 
-public class GameFrame {
-    
+public class GameFrame extends JFrame {
+    public GameFrame(){
+
+        setTitle("Brick Breaker");
+
+        setSize(800, 600);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+
+        setResizable(false);
+
+        add(new GamePanel());
+
+        setVisible(true);
+    }
 }
+
